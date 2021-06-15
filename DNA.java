dcc331ac10f1978ffc6c7115fb3c94d80bdf408e
Muaz Wazir <muazwazir@gmail.com>
2021-06-15 23:50:17 +0800

    merge sort concept

--- DNA.java
+++ DNA.java
@@ -12,24 +12,56 @@ public class DNA {
 
   public static void main(String[] args) {
     setup();
+    readFromInput();
 
-    // for (Character i : map.keySet()) {
+    // for (Integer i : in) {
     // System.out.println(i);
     // }
 
-    readFromInput();
+    // for (String i : input) {
+    // System.out.println(i);
+    // }
+  }
 
-    for (Integer i : in) {
-      System.out.println(i);
-    }
-
+  public static void sort() {
     for (String i : input) {
-      System.out.println(i);
+      DivideAndConquer(i.split(""));
     }
   }
 
-  public static void DivideAndConquer() {
+  public static void DivideAndConquer(String[] data) {
+    if (data.length < 2)
+      return;
 
+    int mid = data.length / 2;
+    String[] l = new String[mid];
+    String[] r = new String[data.length - mid];
+
+    for (int i = 0; i < mid; i++)
+      l[i] = data[i];
+    for (int i = 0; i < data.length; i++)
+      r[i - mid] = data[i];
+
+    DivideAndConquer(l);
+    DivideAndConquer(r);
+
+    merge(data, l, r, mid, data.length - mid);
+  }
+
+  public static void merge(String arr[], String left[], String right[], int l, int r) {
+    int lpoint = 0;
+    int rpoint = 0;
+    int mpoint = 0;
+
+    while (lpoint < l && rpoint < r) {
+      int ldata = map.get((left[lpoint]).charAt(0));
+      int rdata = map.get((right[rpoint]).charAt(0));
+      if (ldata <= rdata)
+        arr[mpoint++] = left[lpoint++];
+      else
+        arr[mpoint++] = right[rpoint++];
+
+    }
   }
 
   public static void BruteForce() {
